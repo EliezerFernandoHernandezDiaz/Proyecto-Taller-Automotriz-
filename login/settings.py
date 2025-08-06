5d04903c331ce6482451ecfa5fa824511181ac10
EliezerFernandoHernandezDiaz <hd23001@ues.edu.sv>
2025-08-06 15:25:45 -0600

    corrección de linea

--- login/settings.py
+++ login/settings.py
@@ -162,9 +162,3 @@ from django.contrib.auth import get_user_model
 
 User= get_user_model(
 )
-if not User.objects.filter(username= 'admin').exists():
-    User.objects.create_superuser(
-        'admin', 
-        'admin@example.com', 
-        'adminpassword15')
-    
